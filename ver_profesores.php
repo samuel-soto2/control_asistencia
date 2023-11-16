ddc01d1c6ab2e83ea90195d680b5edd5d3b0f7f7
samuel-soto2 <1461888@senati.pe>
2023-11-16 11:32:27 -0500

    trabajo final del curso - sistema de control de asistencia

--- ver_profesores.php
+++ ver_profesores.php
@@ -34,3 +34,47 @@ if ($conn->connect_error) {
             <button class="btn btn-primary mt-2" type="submit">Buscar</button>
         </div>
     </form>
+
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>DNI</th>
+                <th>Nombre</th>
+                <th>Eliminar</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['search'])) {
+                $search = $_POST['search'];
+                $sql = "SELECT dni, nombre FROM asistencia WHERE dni LIKE '%$search%' OR nombre LIKE '%$search%' GROUP BY dni";
+            } else {
+                $sql = "SELECT dni, nombre FROM asistencia GROUP BY dni";
+            }
+
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $row["dni"] . "</td>";
+                    echo "<td>" . $row["nombre"] . "</td>";
+                    echo '<td><a href="eliminar_profesor.php?dni=' . $row["dni"] . '" class="btn btn-danger">Eliminar</a></td>';
+                    echo "</tr>";
+                }
+            } else {
+                echo "<tr><td colspan='3'>No se encontraron profesores que coincidan con la búsqueda.</td></tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+    <a class="btn btn-primary" href="panel_admin.php">Volver al Panel de Administrador</a>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+</html>
+
