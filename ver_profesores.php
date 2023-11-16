8b9c0bbb2d27f4c89ed40f553df3f806f05be36a
samuel-soto2 <1461888@senati.pe>
2023-11-16 11:28:34 -0500

    el profesor podra registrarse mientras el admin tendra el control necesario de todo el sistema de asistencia

--- ver_profesores.php
+++ ver_profesores.php
@@ -11,3 +11,26 @@ if ($conn->connect_error) {
     die("Error de conexión a la base de datos: " . $conn->connect_error);
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ver Profesores - Control de Asistencia</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="index.php">Control de Asistencia</a>
+</nav>
+
+<div class="container mt-4">
+    <h2>Ver Profesores</h2>
+    <form action="ver_profesores.php" method="post">
+        <div class="form-group">
+            <label for="search">Filtrar por DNI o Nombre:</label>
+            <input type="text" class="form-control" name="search" id="search" placeholder="Ingrese DNI o Nombre">
+            <button class="btn btn-primary mt-2" type="submit">Buscar</button>
+        </div>
+    </form>
