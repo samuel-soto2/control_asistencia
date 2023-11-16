5eb19c5e95bbbba3d7cc2f53e7b3cfc4569b36ef
samuel-soto2 <1461888@senati.pe>
2023-11-16 10:42:49 -0500

    para poder asegurar en la base de datos lo que hace el admin

---
+++ procesar_agregar_profesor.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Resultado de Agregar Profesor</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <?php
+        // Conexión a la base de datos
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "asistencia";
