737b34af2fa1cf9ee7a7a874d727d93270ceb3a8
samuel-soto2 <1461888@senati.pe>
2023-11-16 10:57:32 -0500

    la conexion a base de datos en el control de asistencia

--- procesar_agregar_profesor.php
+++ procesar_agregar_profesor.php
@@ -13,3 +13,34 @@
         $username = "root";
         $password = "";
         $dbname = "asistencia";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error) {
+            die("Error de conexión a la base de datos: " . $conn->connect_error);
+        }
+
+        if (isset($_POST['nombre']) && isset($_POST['dni'])) {
+            $nombre = $_POST['nombre'];
+            $dni = $_POST['dni'];
+
+            $sql = "INSERT INTO asistencia (nombre, dni) VALUES ('$nombre', '$dni')";
+
+            if ($conn->query($sql) === TRUE) {
+                echo '<div class="alert alert-success" role="alert">Profesor agregado con éxito.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error al agregar al profesor: ' . $conn->error . '</div>';
+            }
+        }
+
+        $conn->close();
+        ?>
+
+        <a href="agregar_profesor.php" class="btn btn-primary">Agregar más profesor</a>
+        <a href="panel_admin.php" class="btn btn-secondary">Volver al panel de admin</a>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
