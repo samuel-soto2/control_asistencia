6747b50e39e2b7a37bf9345a9f18483b0837da9c
samuel-soto2 <1461888@senati.pe>
2023-11-16 09:46:12 -0500

    panel del admin - control de asistencia

--- panel_admin.php
+++ panel_admin.php
@@ -1,1 +1,27 @@
+<?php
+// Inicia la sesión para verificar si el administrador está autenticado
+session_start();
 
+if (!isset($_SESSION['admin']) || !$_SESSION['admin']) {
+    // Si el administrador no está autenticado, redirige a la página de inicio de sesión de administrador
+    header("Location: admin.php");
+    exit();
+}
+
+// Conexión a la base de datos
+$servername = "localhost"; // Cambia esto si tu servidor de base de datos tiene una ubicación diferente
+$username = "root"; // Nombre de usuario de la base de datos (por defecto, en XAMPP es "root")
+$password = ""; // Contraseña de la base de datos (deja en blanco si no tienes una configurada)
+$dbname = "asistencia"; // Nombre de tu base de datos
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verifica la conexión
+if ($conn->connect_error) {
+    die("Error de conexión a la base de datos: " . $conn->connect_error);
+}
+
+// Consulta SQL para obtener todos los registros de asistencia
+$sql = "SELECT * FROM asistencia";
+$result = $conn->query($sql);
+?>
